29fc6213f4c465e679e1b2993d4c5c364785ad2c
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2016-01-14 18:32:56 -0800

    make it compile (WIP)

--- microcoap/main.c
+++ microcoap/main.c
@@ -19,15 +19,25 @@
 
 #include <stdio.h>
 
+/*
 #include "udp.h"
 #include "net_help.h"
 #include "net_if.h"
-#include "periph/cpuid.h" 
+#include "periph/cpuid.h"
 #include "board_uart0.h"
 #include "thread.h"
 #include "posix_io.h"
 #include <coap.h>
 #include "hashes.h"
+*/
+
+#include "thread.h"
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <coap.h>
 
 #define ENABLE_DEBUG    (1)
 #include "debug.h"
@@ -40,17 +50,16 @@
 static void *_microcoap_server_thread(void *arg);
 
 msg_t msg_q[RCV_MSG_Q_SIZE];
-char _rcv_stack_buf[KERNEL_CONF_STACKSIZE_MAIN];
+char _rcv_stack_buf[THREAD_STACKSIZE_MAIN];
 
-static ipv6_addr_t prefix;
+//static ipv6_addr_t prefix;
 int sock_rcv, if_id;
-sockaddr6_t sa_rcv;
+struct sockaddr_in6 sa_rcv;
 uint8_t buf[BUFSZ];
 uint8_t scratch_raw[BUFSZ];
 coap_rw_buffer_t scratch_buf = {scratch_raw, sizeof(scratch_raw)};
 
 static void _init_tlayer(void);
-static uint16_t get_hw_addr(void);
 
 int main(void)
 {
@@ -58,48 +67,56 @@ int main(void)
     DEBUG("Starting example microcoap server...\n");
 
     _init_tlayer();
-    thread_create(_rcv_stack_buf, KERNEL_CONF_STACKSIZE_MAIN, PRIORITY_MAIN, CREATE_STACKTEST, _microcoap_server_thread, NULL ,"_microcoap_server_thread");
+    thread_create(_rcv_stack_buf, sizeof(_rcv_stack_buf), THREAD_PRIORITY_MAIN,
+                  THREAD_CREATE_STACKTEST, _microcoap_server_thread, NULL,
+                  "_microcoap_server_thread");
 
     DEBUG("Ready to receive requests.\n");
 
     return 0;
 }
 
+/*
 static uint16_t get_hw_addr(void)
 {
     return sysconfig.id;
 }
+*/
 
 /* init transport layer & routing stuff*/
+// TODO: do I still need this?
 static void _init_tlayer(void)
 {
     msg_init_queue(msg_q, RCV_MSG_Q_SIZE);
 
+    /*
     net_if_set_hardware_address(0, get_hw_addr());
     DEBUG("set hawddr to: %d\n", get_hw_addr());
 
     printf("initializing 6LoWPAN...\n");
 
     ipv6_addr_init(&prefix, 0xABCD, 0xEF12, 0, 0, 0, 0, 0, 0);
-    if_id = 0; /* having more than one interface isn't supported anyway */
+    if_id = 0; *//* having more than one interface isn't supported anyway */
 
-    sixlowpan_lowpan_init_interface(if_id);
+    //sixlowpan_lowpan_init_interface(if_id);
+
 }
 
 static void *_microcoap_server_thread(void *arg)
 {
-    (void)arg; /* make the compiler shut up about unused variables */
+    (void)arg; /* silence warnings about unused variables */
+
 
     printf("initializing receive socket...\n");
 
-    sa_rcv = (sockaddr6_t) { .sin6_family = AF_INET6,
-               .sin6_port = HTONS(PORT) };
+    sa_rcv = (struct sockaddr_in6) { .sin6_family = AF_INET6,
+                                     .sin6_port = HTONS(PORT) };
 
-    sock_rcv = socket_base_socket(PF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+    sock_rcv = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
 
-    if (-1 == socket_base_bind(sock_rcv, &sa_rcv, sizeof(sa_rcv))) {
+    if (-1 == bind(sock_rcv, (struct sockaddr *) &sa_rcv, sizeof(sa_rcv))) {
         printf("Error: bind to receive socket failed!\n");
-        socket_base_close(sock_rcv);
+        close(sock_rcv);
     }
 
     printf("Ready to receive requests.\n");
@@ -110,7 +127,7 @@ static void *_microcoap_server_thread(void *arg)
         socklen_t len = sizeof(sa_rcv);
         coap_packet_t pkt;
 
-        n = socket_base_recvfrom(sock_rcv, buf, sizeof(buf), 0, &sa_rcv, &len);
+        n = recvfrom(sock_rcv, buf, sizeof(buf), 0, (struct sockaddr*) &sa_rcv, &len);
         printf("Received packet: ");
         coap_dump(buf, n, true);
         printf("\n");
@@ -134,7 +151,7 @@ static void *_microcoap_server_thread(void *arg)
                 printf("\n");
                 printf("content:\n");
                 coap_dumpPacket(&rsppkt);
-                socket_base_sendto(sock_rcv, buf, rsplen, 0, &sa_rcv, sizeof(sa_rcv));
+                sendto(sock_rcv, buf, rsplen, 0, (struct sockaddr*) &sa_rcv, sizeof(sa_rcv));
             }
         }
     }
