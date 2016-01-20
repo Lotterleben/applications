77c15b1c7f499dce70989513863f2c8fd9b2c705
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2016-01-20 06:47:11 -0800

    add shell

--- microcoap/main.c
+++ microcoap/main.c
@@ -19,19 +19,9 @@
 
 #include <stdio.h>
 
-/*
-#include "udp.h"
-#include "net_help.h"
-#include "net_if.h"
-#include "periph/cpuid.h"
-#include "board_uart0.h"
+#include "shell.h"
 #include "thread.h"
-#include "posix_io.h"
-#include <coap.h>
-#include "hashes.h"
-*/
-
-#include "thread.h"
+#include "msg.h"
 
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -39,8 +29,8 @@
 
 #include <coap.h>
 
-#define ENABLE_DEBUG    (1)
-#include "debug.h"
+//#define ENABLE_DEBUG    (1)
+//#include "debug.h"
 
 #define PORT 5683
 #define BUFSZ 128
@@ -48,70 +38,39 @@
 #define RCV_MSG_Q_SIZE      (64)
 
 static void *_microcoap_server_thread(void *arg);
+int listen_cmd(int argc, char **argv);
+
+static const shell_command_t shell_commands[] = {
+    { "listen", "listen for coap requests", listen_cmd },
+    { NULL, NULL, NULL }
+};
+
 
 msg_t msg_q[RCV_MSG_Q_SIZE];
 char _rcv_stack_buf[THREAD_STACKSIZE_MAIN];
 
-//static ipv6_addr_t prefix;
 int sock_rcv, if_id;
 struct sockaddr_in6 sa_rcv;
 uint8_t buf[BUFSZ];
 uint8_t scratch_raw[BUFSZ];
 coap_rw_buffer_t scratch_buf = {scratch_raw, sizeof(scratch_raw)};
 
-static void _init_tlayer(void);
-
-int main(void)
-{
-
-    DEBUG("Starting example microcoap server...\n");
-
-    _init_tlayer();
-    thread_create(_rcv_stack_buf, sizeof(_rcv_stack_buf), THREAD_PRIORITY_MAIN,
-                  THREAD_CREATE_STACKTEST, _microcoap_server_thread, NULL,
-                  "_microcoap_server_thread");
-
-    DEBUG("Ready to receive requests.\n");
-
-    return 0;
-}
-
-/*
-static uint16_t get_hw_addr(void)
-{
-    return sysconfig.id;
-}
-*/
 
 /* init transport layer & routing stuff*/
 // TODO: do I still need this?
 static void _init_tlayer(void)
 {
     msg_init_queue(msg_q, RCV_MSG_Q_SIZE);
-
-    /*
-    net_if_set_hardware_address(0, get_hw_addr());
-    DEBUG("set hawddr to: %d\n", get_hw_addr());
-
-    printf("initializing 6LoWPAN...\n");
-
-    ipv6_addr_init(&prefix, 0xABCD, 0xEF12, 0, 0, 0, 0, 0, 0);
-    if_id = 0; *//* having more than one interface isn't supported anyway */
-
-    //sixlowpan_lowpan_init_interface(if_id);
-
 }
 
 static void *_microcoap_server_thread(void *arg)
 {
     (void)arg; /* silence warnings about unused variables */
 
-
     printf("initializing receive socket...\n");
 
     sa_rcv = (struct sockaddr_in6) { .sin6_family = AF_INET6,
                                      .sin6_port = HTONS(PORT) };
-
     sock_rcv = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
 
     if (-1 == bind(sock_rcv, (struct sockaddr *) &sa_rcv, sizeof(sa_rcv))) {
@@ -158,3 +117,40 @@ static void *_microcoap_server_thread(void *arg)
 
     return NULL;
 }
+
+int listen_cmd(int argc, char **argv)
+{
+    (void)argc; /* silence warnings about unused variables */
+    (void)argv;
+
+    int ret = 0;
+    printf("Starting example microcoap server...\n");
+
+    if (thread_create(_rcv_stack_buf, sizeof(_rcv_stack_buf), THREAD_PRIORITY_MAIN -1,
+                      THREAD_CREATE_STACKTEST, _microcoap_server_thread, NULL,
+                      "_microcoap_server_thread") >= 0)
+    {
+        printf("Ready to receive requests.\n");
+    }
+    else
+    {
+        printf("Error! :(\n");
+        ret = 1;
+    }
+
+    return ret;
+}
+
+int main(void)
+{
+    printf("Welcome to the microcoap example application! \nTo start listening for "
+           "requests, enter 'listen'\n");
+    _init_tlayer();
+
+    /* Start the shell */
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
+
